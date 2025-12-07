94b4efb1a87f0a82e9f348d6e058ad0e95a02736
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-07 20:23:17 +0800

    added loading transition

--- client/src/components/loading-overlay.vue
+++ client/src/components/loading-overlay.vue
@@ -1,20 +1,29 @@
 <template>
-  <div
-    v-if="loading"
-    v-motion-fade
-    class="absolute z-100 top-0 left-0 w-screen h-screen bg-black/60 flex gap-2 flex-col items-center justify-center"
+  <Transition
+    enter-active-class="transition-opacity duration-300"
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="transition-opacity duration-300"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
   >
-    <vue-spinner-sync color="white"></vue-spinner-sync>
-    <small class="text-white">Please wait...</small>
-  </div>
+    <div
+      v-show="loading"
+      class="absolute z-100 top-0 left-0 w-screen h-screen bg-black/60 flex gap-2 flex-col items-center justify-center"
+    >
+      <vue-spinner-sync color="white"></vue-spinner-sync>
+      <small class="text-white">Please wait...</small>
+    </div>
+  </Transition>
 </template>
 
 <script>
 import { VueSpinnerSync } from "vue3-spinners";
-
+import { Transition } from "vue";
 export default {
   components: {
     VueSpinnerSync,
+    Transition,
   },
 
   props: {
