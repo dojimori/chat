c730b3dfe1c9121241164ad791b0830cac69d752
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2025-12-10 21:38:50 +0800

    remove vue devtools

--- client/src/components/active-users.vue
+++ client/src/components/active-users.vue
@@ -5,18 +5,6 @@
       <h2 class="header p-2 font-bold text-white text-center">Active Users</h2>
       <div class="flex flex-col">
         <!-- TODO: replace with your actual user list -->
-        <div class="flex items-center cursor-pointer gap-2 p-2">
-          <!-- <div class="w-8 h-8 bg-blue-200 rounded-full"></div> -->
-          <img src="/def_pfp_2.jpg" class="pfp" />
-          <span class="text-xs">nko</span>
-
-          <span class="w-2 h-2 rounded-full bg-green-600"></span>
-        </div>
-        <div class="flex items-center gap-2 cursor-pointer p-2">
-          <img src="/def_pfp_3.jpg" class="pfp" />
-          <span class="text-xs">yohiori</span>
-          <span class="w-2 h-2 rounded-full bg-green-600"></span>
-        </div>
       </div>
     </div>
   </div>
