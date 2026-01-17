239c374ea9d062f464696327dea4e2cee3267f9c
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2026-01-17 20:39:43 +0800

    feat: add user test

--- server/src/__tests__/services/user.service.test.ts
+++ server/src/__tests__/services/user.service.test.ts
@@ -1,5 +1,34 @@
-describe('user service', () => {
-  
+import { UserService } from "../../modules/users/user.service";
+import { UserRepository } from "../../modules/users/user.repository";
 
+const mockUserRepo = {
+  findByUsername: jest.fn()
+}
+
+describe('user service', () => {
+  let userService: UserService;
+  beforeEach(() => {
+    userService = new UserService(mockUserRepo as any)
+  })
+
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+
+
+  it('should return a user by with the username', async () => {
+    mockUserRepo.findByUsername.mockResolvedValue({
+      id: 1,
+      username: 'doujin22',
+      password: 'hashed'
+    })
+
+    const response = await userService.findByUsername('doujin22');
+
+    expect(response).toMatchObject({
+      username: 'doujin22'
+    })
+  })
 
 })
--- server/src/modules/users/user.interface.ts
+++ server/src/modules/users/user.interface.ts
@@ -2,7 +2,7 @@ import { Profile, User } from "../../../generated/prisma/client";
 import { CreateUserDto } from "./dto/create.dto";
 
 export interface IUserRepository {
-  create(payload: CreateUserDto ): Promise<User>;
+  create(payload: CreateUserDto): Promise<User>;
   findByUsername(username: string): Promise<User | null>;
   findById(id: number): Promise<User | null>;
   findByIdWithProfile(id: number): Promise<User | null>;
