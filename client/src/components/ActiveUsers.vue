c8ff492f9f79bba0c5d993002ebca513d1fa9dd5
Jyn-Tuyor <tuyor.jynchris@gmail.com>
2026-01-22 10:52:08 +0800

    restyle: remove the x axis transition in activeusers component

--- client/src/components/ActiveUsers.vue
+++ client/src/components/ActiveUsers.vue
@@ -1,28 +1,19 @@
 <template>
   <!-- Users List Section -->
-  <div v-motion-slide-left class="bg-white shadow-lg lg:w-[170px]">
+  <div v-motion-fade class="bg-white shadow-lg lg:w-[170px]">
     <div>
       <h2 class="header p-2 font-bold text-white text-center">Active Users</h2>
       <div class="flex flex-col">
         <!-- TODO: this will be clickable and redirect to user's profile page :) -->
-        <div
-          v-for="user in activeUsers"
-          class="p-2 flex gap-5 hover:bg-gray-100 items-center cursor-pointer"
-        >
-          <img
-            :src="
-              user.profilePicture
-                ? `http://localhost:8080${user.profilePicture}`
-                : '/def_pfp_6.jpg'
-            "
-            class="pfp border-2 border-gray-400"
-          />
+        <div v-for="user in activeUsers" class="p-2 flex gap-5 hover:bg-gray-100 items-center cursor-pointer">
+          <img :src="user.profilePicture
+              ? `http://localhost:8080${user.profilePicture}`
+              : '/def_pfp_6.jpg'
+            " class="pfp border-2 border-gray-400" />
 
           <h4>{{ user.username }}</h4>
 
-          <div
-            class="w-[10px] h-[10px] border-2 border-green-100 rounded-full bg-green-500"
-          ></div>
+          <div class="w-[10px] h-[10px] border-2 border-green-100 rounded-full bg-green-500"></div>
         </div>
       </div>
     </div>
